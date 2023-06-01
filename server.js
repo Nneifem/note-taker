2a573b4ebed5018c8fb4cc8a98fe342eb0dc58d9
Nneif <nneifemakpunonu@gmail.com>
2023-06-01 11:48:42 -0400

    got the start page to go to the note taking section after clicking the button

--- server.js
+++ server.js
@@ -1,18 +1,25 @@
 const express = require('express');
+const path = require('path');
 
-const PORT = 7007;
-const db = require('./db/db.json');
+const PORT = 3007;
+const noteDb = require('./db/db.json');
 
 const app = express();
 
-// middleware to parse application/json
+// // middleware to parse application/json
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => 
-    res.sendFile(path.join(__dirname, '/public/notes.html'))
-);
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
 
-// GET requests for notes
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
+app.listen(PORT, () => 
+    console.log(`Note taker is ready at http://localhost:${PORT}`)
+); 
